45534afdad4e54140f72f6e0de956bb65902244c
Daniel <danisan9490@gmail.com>
2020-09-20 18:46:58 +0200

    Updates

---
+++ precache-manifest.18ff8ea3ddd7ea6549c83a5e89843e67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421a6167afa12979e1fc49dfe5f7741a",
+    "url": "/roboCards-redux/index.html"
+  },
+  {
+    "revision": "f1326a25f4896c4039ed",
+    "url": "/roboCards-redux/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c83b9180a98f761ebf5b",
+    "url": "/roboCards-redux/static/css/main.877de943.chunk.css"
+  },
+  {
+    "revision": "f1326a25f4896c4039ed",
+    "url": "/roboCards-redux/static/js/2.e279aa7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/roboCards-redux/static/js/2.e279aa7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83b9180a98f761ebf5b",
+    "url": "/roboCards-redux/static/js/main.a278213b.chunk.js"
+  },
+  {
+    "revision": "a6d93c53eb209666c0fa",
+    "url": "/roboCards-redux/static/js/runtime-main.761f78f3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboCards-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/roboCards-redux/precache-manifest.18ff8ea3ddd7ea6549c83a5e89843e67.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/roboCards-redux/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
