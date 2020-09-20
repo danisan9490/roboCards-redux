0356bc65bb397aa19633fd83ce53d3504cdb4035
Daniel <danisan9490@gmail.com>
2020-09-20 15:25:08 +0200

    #5 Create Redux Rreducer Search

---
+++ roboCards-redux/src/reducers.js
@@ -0,0 +1,15 @@
+import { CHANGE_SEARCH_FIELD } from './constans'
+
+
+const initialState = {
+  searchField: ''
+}
+
+export const searchRobotReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case CHANGE_SEARCH_FIELD:
+      return Object.assign({}, state, { searchField: action.payload })
+    default:
+      return state;
+  }
+}
